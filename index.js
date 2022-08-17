c05e334371bc1ae628cdfd24f069acacc1b5d1ad
Sam Avery <sam.avery1@gmail.com>
2022-08-17 16:59:17 -0500

    commit tree 38cda3d8dae5c2713d019d099935b230259649a7

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(name){
+    return (`Hi, my name is ${name}.`);
+}
+
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+
+  function introductionWithLanguage (name, language){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+function introductionWithLanguageOptional (value1, value2="JavaScript") {
+    return(`Hi, my name is ${value1} and I am learning to program in ${value2}.`);
+  }
+  
